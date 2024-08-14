dab0aa3dc264c22d0173603f524e8db6bf0ef989
nileshp-07 <patidarnilesh7223@gmail.com>
2024-08-14 21:58:14 +0530

    feat - stripe implementation #2

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -1,14 +1,20 @@
-import stripe from 'stripe'
+import Stripe from 'stripe'
 import { NextResponse } from 'next/server'
 import { createOrder } from '@/lib/actions/order.action'
 
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
 export async function POST(request: Request) {
+
+  console.log("Webhook hit");
   const body = await request.text()
+
+  console.log(body);
 
   const sig = request.headers.get('stripe-signature') as string
   const endpointSecret = process.env.STRIPE_WEBHOOK_SECRET!
 
-  let event
+  let event : Stripe.Event;
 
   try {
     event = stripe.webhooks.constructEvent(body, sig, endpointSecret)
@@ -21,15 +27,15 @@ export async function POST(request: Request) {
 
   // CREATE
   if (eventType === 'checkout.session.completed') {
-    const { id, amount_total, metadata } = event.data.object
+    const session = event.data.object as Stripe.Checkout.Session;
 
     const order = {
-      orderId : id,
-      eventId: metadata?.eventId || '',
-      userId: metadata?.buyerId || '',
-      amount: amount_total ? (amount_total / 100).toString() : '0',
+      orderId: session.id,
+      eventId: session.metadata?.eventId || '',
+      userId: session.metadata?.buyerId || '',
+      amount: session.amount_total ? (session.amount_total / 100).toString() : '0',
       createdAt: new Date(),
-    }
+    };
 
     const newOrder = await createOrder(order)
     return NextResponse.json({ message: 'OK', order: newOrder })
